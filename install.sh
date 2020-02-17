f880d03b65339ad4e7186b1338fa9cd8289f4fd3
wm_pc_2 <wmaking@ens.etsmtl.ca>
2020-02-17 18:59:05 -0500

    Make the install script execute itself when compiling

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-predictor=68
-if [ $# -eq 1 ]; then
-    predictor=$1
+if [ -f scripts/data/shape_predictor_68_face_landmarks.dat ]
+then
+
+  echo "ROS face detection is already instaled."
+
+else
+
+  predictor=68
+  if [ $# -eq 1 ]; then
+      predictor=$1
+  fi
+
+  wget -P ./scripts/data "http://dlib.net/files/shape_predictor_$(echo $predictor)_face_landmarks.dat.bz2"
+  wget -P ./scripts/data "http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2"
+
+  bzip2 -d ./scripts/data/*.bz2
+
 fi
-
-wget -P ./scripts/data "http://dlib.net/files/shape_predictor_$(echo $predictor)_face_landmarks.dat.bz2"
-wget -P ./scripts/data "http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2"
-
-bzip2 -d ./scripts/data/*.bz2
-
